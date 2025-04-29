a5bda3a253e7c3c6e1be374e85af95683eadc0cb
Wayne Siddall <nathaniel.siddall@gmail.com>
2025-04-29 12:31:12 -0500

    fixing some other problems

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom';
-import HomePage from './pages/HomePage';
-import DesignPage from './pages/DesignPage';
-import OtherPage from './pages/OtherPage';
-import TopNav from './pages/components/TopNav';
-import GalleryPage from "./pages/GalleryPage";
+import { BrowserRouter as Router, Routes, Route, Navigate } from 'react-router-dom';
+import HomePage    from './pages/HomePage';
+import DesignPage  from './pages/DesignPage';
+import GalleryPage from './pages/GalleryPage';
+import OtherPage   from './pages/OtherPage';
+import TopNav      from './pages/components/TopNav';
 
-
-
-
-class App extends React.Component {
+export default class App extends React.Component {
     render() {
         return (
-            <Router>
-                <div>
-                    <TopNav />
-                    <div className="App-content">
-                        <Routes>
-                            <Route path="/" element={<HomePage />} />
-                            <Route path="/design" element={<DesignPage />} />
-                            <Route path="/gallery" element={<GalleryPage />} />
-                            <Route path="/" element={<OtherPage />} />
-                        </Routes>
-                    </div>
+            <Router basename={process.env.PUBLIC_URL}>
+                <TopNav />
+                <div className="App-content">
+                    <Routes>
+                        <Route path="/"        element={<HomePage />}    />
+                        <Route path="/design"  element={<DesignPage />}  />
+                        <Route path="/gallery" element={<GalleryPage />} />
+                        <Route path="/other"   element={<OtherPage />}   />
+                        <Route path="*"        element={<Navigate to="/" replace />} />
+                    </Routes>
                 </div>
             </Router>
         );
     }
 }
-
-export default App;
